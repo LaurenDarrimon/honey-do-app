172958a5bad1b9ff8ea3217d3420308570d64d8a
LaurenDarrimon <laurendarrimon@gmail.com>
2022-10-19 17:03:46 -0700

    rendering for single event page with dynamic url

---
+++ pages/event/[id].js
@@ -0,0 +1,75 @@
+import React from "react";
+
+import {
+    Box,
+    Heading,
+    Text
+} from "@chakra-ui/react";
+
+import useAuth from "../../hooks/useAuth";
+
+import { doc, getDoc } from "firebase/firestore";
+
+import { db } from "../../firebase" //will default to index.js in firebase directory
+
+//define JSX component to render single event
+
+const EventItem = ({itemData}) =>{
+    //enforce user login 
+    //use Auth is imported hook, we should get back user object from it 
+    const { user } = useAuth() || {};
+
+    if(!user){
+        //if no user, bail out
+        return;
+    }
+
+    //return jsx 
+    return (
+        <Box mt={(5)}>
+            <Heading as="h3" fontSize={"xl"}>
+                {itemData.title}
+            </Heading>
+            <Text>
+                {itemData.description}
+            </Text>
+            <Text>
+                {itemData.eventDate}
+            </Text>
+            <Text>
+                {itemData.status}
+            </Text>
+            <Text>
+                {itemData.createdAt}
+            </Text>
+
+        </Box>
+    );
+
+
+};
+
+//define required get server side props function that Next will call whenever it gets a 
+//dynamically routed url
+export async function getServerSideProps(context){
+    //this function will get what it needs to know from Next.js on the backend in the context variable
+    //if we want url paramerter from Next.js sent for ID, context.params.id
+
+    let itemData = null;
+    //get doc from firestore
+    const docRef = doc( db, "event", context.params.id)
+
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()){
+        itemData = docSnap.data()
+    };
+
+    return {
+        props: {
+            itemData
+        }
+    };
+}
+
+export default EventItem; 
